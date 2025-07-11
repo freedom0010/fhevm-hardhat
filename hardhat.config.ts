50683055227ca02b7e69fc91da991c986898159c
freedom0010 <162872021+freedom0010@users.noreply.github.com>
2025-07-11 15:53:33 +0800

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,40 +3,16 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const MNEMONIC: string = process.env.MNEMONIC || "test test test test test test test test test test test junk";
-const INFURA_API_KEY: string = process.env.INFURA_API_KEY || "";
-const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY || "";
+const MNEMONIC: string = process.env.MNEMONIC || "";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
-  namedAccounts: {
-    deployer: 0,
-  },
-  etherscan: {
-    apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
-    },
-  },
-  gasReporter: {
-    currency: "USD",
-    enabled: process.env.REPORT_GAS ? true : false,
-    excludeContracts: [],
-  },
   networks: {
     hardhat: {
       accounts: {
         mnemonic: MNEMONIC,
       },
       chainId: 31337,
-    },
-    anvil: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
-      chainId: 31337,
-      url: "http://localhost:8545",
     },
     sepolia: {
       accounts: {
@@ -45,31 +21,17 @@ const config: HardhatUserConfig = {
         count: 10,
       },
       chainId: 11155111,
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      url: "https://fhe-sepolia.zama.ai",
     },
-  },
-  paths: {
-    artifacts: "./artifacts",
-    cache: "./cache",
-    sources: "./contracts",
-    tests: "./test",
   },
   solidity: {
     version: "0.8.24",
     settings: {
-      metadata: {
-        bytecodeHash: "none",
-      },
       optimizer: {
         enabled: true,
         runs: 800,
       },
-      evmVersion: "cancun",
     },
-  },
-  typechain: {
-    outDir: "types",
-    target: "ethers-v6",
   },
 };
 
